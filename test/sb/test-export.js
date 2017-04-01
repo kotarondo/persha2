ecfa80d23ff12378be986384eecee1d5a7944f97
Kotaro Endo <kotarondo@gmail.com>
2017-04-01 11:18:25 +0900

    node>=6.5 because of use of Proxy

--- test/sb/test-export.js
+++ test/sb/test-export.js
@@ -31,4 +31,67 @@ try {
     assert_equals(e.toString(), 'SyntaxError: test.js:1:5');
 }
 
+function test() {
+    var proto = [];
+    var obj = Object.create(proto);
+    Object.defineProperty(obj, "A", {
+        value: 'A',
+        writable: true,
+        enumerable: true,
+        configurable: true,
+    });
+    Object.defineProperty(proto, "B", {
+        value: 'B2',
+        writable: true,
+        enumerable: true,
+        configurable: true,
+    });
+    Object.defineProperty(obj, "B", {
+        value: 'B',
+        writable: true,
+        enumerable: false,
+        configurable: true,
+    });
+    Object.defineProperty(proto, "C", {
+        value: 'C2',
+        writable: true,
+        enumerable: true,
+        configurable: true,
+    });
+    Object.defineProperty(obj, "C", {
+        get: function() {
+            return "C"
+        },
+        enumerable: true,
+        configurable: true,
+    });
+    Object.defineProperty(proto, "D", {
+        value: 'D',
+        writable: true,
+        enumerable: true,
+        configurable: true,
+    });
+    Object.defineProperty(proto, "E", {
+        value: 'E',
+        writable: false,
+        enumerable: false,
+        configurable: true,
+    });
+    Object.defineProperty(proto, "F", {
+        get: function() {
+            return "F"
+        },
+        enumerable: true,
+        configurable: true,
+    });
+    return obj;
+}
+
+var result = sandbox.evaluateProgram("(" + test + ")()", "test.js");
+assert_equals(Object.getOwnPropertyNames(result).toString(), 'A,B,C');
+assert_equals(Object.keys(result).toString(), 'A,C');
+var x = [];
+for (var P in result) x.push(P, result[P]);
+assert_equals(x.toString(), 'A,A,C,,D,D,F,');
+
 test_success()
